1799b739793e2e828cb3a222999a7545805506a2
Ethan Turk <eternally1027@gmail.com>
2023-06-15 23:31:07 -0500

    Update wheel

--- api/Triggers/GetNames.cs
+++ api/Triggers/GetNames.cs
@@ -17,7 +17,7 @@ public class GetNames
     }
     
     [Function("GetNames")]
-    public async Task<HttpResponseData> Run([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequestData req,
+    public async Task<HttpResponseData> Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequestData req,
         FunctionContext executionContext)
     {
         var logger = executionContext.GetLogger("GetNames");
--- src/App.js
+++ src/App.js
@@ -1,35 +1,66 @@
 import React from 'react';
-import { useState } from "react";
+import { useState, useEffect  } from "react";
 import Winwheel from 'javascript-winwheel-react'
 
 function App() {
   const [theWheel, setTheWheel] = useState();
+  const [names, setNames] = useState();
+  const [wheelContents, setWheelContents] = useState();
+
+  var colors = ["#eae56f", "#89f26e", "#7de6ef", "#e7706f"]
+
+  var goOnce = ""
+
+  useEffect(() => {
+    buildWheel()
+  }, [goOnce])
+
+  function getAllNames() {
+    return fetch('http://127.0.0.1:4280/api/GetNames?sessionName=Session1')
+      //.then(result => { setNames(result) })
+  }
+
+  function alertWinner(indicatedSegment) {
+    alert("You have won " + indicatedSegment.text);
+  }
+
+  async function buildWheel() {
+    await getAllNames().then(async (ns) => {
+      var names = ns.json()
+      var wheelContents = []
+      var colorNumber = 0
+      var namesFinished = await names
+      for (var i = 0; i < namesFinished.length; i++) {
+        wheelContents.push({'fillStyle' : colors[colorNumber], 'text' : namesFinished[i].RowKey})
+        if (colorNumber == 3) { colorNumber = 0 }
+        colorNumber++
+      };
+      setWheelContents(wheelContents)
+    })
+  }
   
   return <div className="App">
-    <Winwheel 
-      width='300'
-      height='300'
-      segments={[
-        {'fillStyle' : '#eae56f', 'text' : 'Prize 1'},
-        {'fillStyle' : '#89f26e', 'text' : 'Prize 2'},
-        {'fillStyle' : '#7de6ef', 'text' : 'Prize 3'},
-        {'fillStyle' : '#e7706f', 'text' : 'Prize 4'},
-        {'fillStyle' : '#eae56f', 'text' : 'Prize 5'},
-        {'fillStyle' : '#89f26e', 'text' : 'Prize 6'},
-        {'fillStyle' : '#7de6ef', 'text' : 'Prize 7'},
-        {'fillStyle' : '#e7706f', 'text' : 'Prize 8'}
-      ]}
-      animation={{
-          'type'     : 'spinToStop',
-          'duration' : 5,
-          'spins'    : 8
-      }}
-      ref={setTheWheel}
-    ></Winwheel>
-
-    <button onClick={()=>theWheel.spin()}>
-      spin
-    </button>
+    <div className="thewheel" width="500" onClick={()=>theWheel.spin()}>
+      <Winwheel 
+        width='440'
+        height='500'
+        outerRadius='212'
+        innerRadius='75'
+        segments={wheelContents}
+        animation={{
+            'type'     : 'spinToStop',
+            'duration' : 8,
+            'spins'    : 10,
+            'callbackFinished': alertWinner
+        }}
+        pins={{
+          'number'     : 24,
+          'fillStyle'  : 'silver',
+          'outerRadius': 4,
+        }}
+        ref={setTheWheel}
+      ></Winwheel>
+    </div>
   </div>;
 }
 
