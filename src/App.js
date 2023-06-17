fc16c900e8ed4632363eed401395725f5f6a7f2d
Ethan Turk <eternally1027@gmail.com>
2023-06-17 00:42:50 -0500

    Tweaks

--- src/App.js
+++ src/App.js
@@ -33,30 +33,41 @@ function App() {
     fetchData()
   }, [goOnce])
   
-  return <div className="App">
-    <div className="thewheel" width="500" onClick={()=>theWheel.spin()}>
-      <Winwheel 
-        width='440'
-        height='500'
-        outerRadius='212'
-        innerRadius='75'
-        segments={wheelContents}
-        animation={{
-            'type'     : 'spinToStop',
-            'duration' : 8,
-            'spins'    : 10,
-            'callbackFinished': alertWinner
-        }}
-        pins={{
-          'number'     : 24,
-          'fillStyle'  : 'silver',
-          'outerRadius': 4,
-        }}
-        ref={setTheWheel}
-      ></Winwheel>
+  return <div className="App" class="container">
+    <div class="row">
+      <div className="thewheel col" onClick={()=>theWheel.spin()}>
+        <Winwheel 
+          width='440'
+          height='500'
+          class='column'
+          outerRadius='212'
+          innerRadius='75'
+          segments={wheelContents}
+          animation={{
+              'type'     : 'spinToStop',
+              'duration' : 8,
+              'spins'    : 10,
+              'callbackFinished': alertWinner
+          }}
+          pins={{
+            'number'     : 24,
+            'fillStyle'  : 'silver',
+            'outerRadius': 4,
+          }}
+          ref={setTheWheel}
+        ></Winwheel>
+      </div>
+      <div class="col">
+        <br />
+        <textarea title="Names" placeholder="Names" name="names" class="form-control align-middle" rows={16}></textarea>
+        <br />
+        <span>
+          <button class="form-control">Randomize</button>
+          <button class="form-control">Update</button>
+        </span>
+      </div>
     </div>
-    <div>
-      <textarea name="names" rows={10} cols={40}></textarea>
+    <div class="row">
     </div>
   </div>;
 }
