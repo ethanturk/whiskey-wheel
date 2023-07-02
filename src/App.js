8667580fe02b069db754f9851fd1fa102ed712c2
ethanturk <ethan.turk@gmail.com>
2023-07-01 23:09:42 -0500

    Style it up a bit

--- src/App.js
+++ src/App.js
@@ -9,11 +9,21 @@ function App() {
   const [wheelReadyToSpin, setWheelReadyToSpin] = useState(true)
   const [names, setNames] = useState('')
   const [winner, setWinner] = useState('')
+  const [sessionName, setSessionName] = useState('')
   const [show, setShow] = useState(false)
+  const [clipboardIndicator, setClipboardIndicator] = useState({})
   const wheelColors = ["#eae56f", "#89f26e", "#7de6ef", "#e7706f"]
 
   const handleNameChange = e => {
     populateWheel(e.target.value)
+  }
+
+  const copySessionInfo = async e => {
+    await navigator.clipboard.writeText(e.target.value)
+    await clipboardIndicator.classList.add('green')
+    setTimeout(() => {
+      clipboardIndicator.classList.remove('green');
+    }, 5000)
   }
 
   function alertWinner(indicatedSegment) {
@@ -24,7 +34,6 @@ function App() {
   }
 
   function spinWheel(override) {
-    console.log(wheelReadyToSpin)
     if (wheelReadyToSpin || override === true) {
       theWheel.spin()
       setWheelReadyToSpin(false)
@@ -99,7 +108,7 @@ function App() {
   }
 
   async function updateNames() {
-    var sessionName = getSessionName()
+    getSessionName()
     await fetch(`${window.location.origin}/api/UpdateNames`, {
       method: 'POST',
       headers: {
@@ -111,13 +120,13 @@ function App() {
 
   function getSessionName() {
     const queryParams = new URLSearchParams(window.location.search)
-    return queryParams.get("session")
+    setSessionName(queryParams.get("session"))
   }
 
   /* eslint-disable */
   useEffect(() => {
+    getSessionName()
     async function fetchData() {
-      var sessionName = getSessionName()
       await fetch(`${window.location.origin}/api/GetNames?sessionName=${sessionName}`)
         .then(async (ns) => {
           var localNames = await ns.text()
@@ -126,27 +135,48 @@ function App() {
         })
       }
     fetchData()
-  }, [])
+  }, [sessionName])
   /* eslint-disable */
   
   return <div className="App container">
     <div className="row">
-      <div className="thewheel col" onClick={spinWheel}>
-        <Winwheel 
-          width='440'
-          height='500'
-          class='column'
-          outerRadius='212'
-          innerRadius='75'
-          segments={wheelContents}
-          animation={{
-              'type'     : 'spinToStop',
-              'duration' : 8,
-              'spins'    : 10,
-              'callbackFinished': alertWinner
-          }}
-          ref={setTheWheel}
-        ></Winwheel>
+      <div className="thewheel col">
+        <div onClick={spinWheel}>
+          <Winwheel
+            width='440'
+            height='500'
+            class='column'
+            outerRadius='212'
+            innerRadius='75'
+            segments={wheelContents}
+            animation={{
+                'type'     : 'spinToStop',
+                'duration' : 8,
+                'spins'    : 10,
+                'callbackFinished': alertWinner
+            }}
+            ref={setTheWheel}
+          ></Winwheel>
+        </div>
+        <br />
+        <label class="text-light">Session Name</label>
+        <div className="input-group mb-3">
+          <input 
+            type="text"
+            readOnly={true}
+            label="Session Name"
+            className="form-control bg-dark bg-gradient text-light" 
+            value={`${window.location.origin}?session=${sessionName}`}
+            onChange={setSessionName}
+            onClick={copySessionInfo} />
+          <div className="input-group-append">
+            <span 
+              className="input-group-text" 
+              id="basic-addon2">
+                <i className="bi bi-clipboard-check-fill" ref={setClipboardIndicator}></i>
+              </span>
+          </div>
+        </div>
       </div>
       <div className="col">
         <br />
@@ -154,15 +184,16 @@ function App() {
           title="Names" 
           placeholder="Names" 
           name="namesBox" 
-          className="form-control align-middle" 
+          className="form-control align-middle bg-dark bg-gradient text-light" 
           rows={16}
           value={names}
           onChange={handleNameChange} 
           />
         <br />
         <span>
-          <button className="form-control" onClick={randomizeNames}>Randomize</button>
-          <button className="form-control" onClick={updateNames}>Update</button>
+          <button className="form-control btn btn-dark" onClick={spinWheel}>Spin the Wheel</button>
+          <button className="form-control btn btn-dark" onClick={randomizeNames}>Randomize</button>
+          <button className="form-control btn btn-dark" onClick={updateNames}>Update</button>
         </span>
       </div>
     </div>
