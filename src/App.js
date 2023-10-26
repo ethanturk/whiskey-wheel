3dfea1ca44f1106dd7bb69fca8349a5de77a1c6a
ethanturk <ethan.turk@gmail.com>
2023-10-25 20:21:18 -0500

    Use a better randomizer algorithm

--- src/App.js
+++ src/App.js
@@ -86,12 +86,10 @@ function App() {
   }
 
   function shuffleArray(array) {
-    for (let i = array.length - 1; i > 0; i--) {
-      const j = Math.floor(Math.random() * (i + 1))
-      const temp = array[i]
-      array[i] = array[j]
-      array[j] = temp
-    }
+    for (let i = array.length - 1; i > 0; i--) { 
+      const j = Math.floor(Math.random() * (i + 1)); 
+      [array[i], array[j]] = [array[j], array[i]]; 
+    } 
     return array
   }
 
